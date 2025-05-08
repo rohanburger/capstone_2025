7dba2c3eac0dd18e712f7d76f0bb4a74458c44c3
rjanz <rjanzenburger@gmail.com>
2025-05-08 11:47:19 +0200

    Sesssion and payment domain update

--- src/main/java/za/ac/cput/domain/Payment.java
+++ src/main/java/za/ac/cput/domain/Payment.java
@@ -1,4 +1,56 @@
 package za.ac.cput.domain;
-
+/*
+    Payment.java
+    Payment builder Class
+    Author:Rohan Janzen Burger 230171990
+    Date:19/03/2025
+*/
 public class Payment {
+    private String paymentid;//Initialize all neccesary attributes for the Payment class
+    private Float paymentamount;
+
+    private Payment() {//Default constructor
+    }
+
+    private Payment(Builder builder) {//Constructor with he paramenter of the builder
+        this.paymentid = builder.paymentid;//The Builder attributes are assigned the values from the builder pattern
+        this.paymentamount = builder.paymentamount;
+    }
+
+    public String getPaymentid() {//Get method retrieving the value
+        return paymentid;
+    }
+
+    public Float getPaymentamount() {
+        return paymentamount;
+    }
+
+    @Override
+    public String toString() {//To String displays the attribute values as a string
+        return "Payment{" +
+                "paymentid='" + paymentid + '\'' +
+                ", paymentamount=" + paymentamount +
+                '}';
+    }
+
+    public static class Builder{//Builder class contained inside of the domain class
+        private String paymentid;//Attributes for the builder class
+        private Float paymentamount;
+
+        public Builder setPaymentid(String paymentid) {//Method with paramters that sets the value of the attribute ,expects a return of type builder
+            this.paymentid = paymentid;
+            return this;
+        }
+
+        public Builder setPaymentamount(Float paymentamount) {
+            this.paymentamount = paymentamount;
+            return this;
+        }
+
+        public Payment build(){//Method that returns/builds a type Payment
+            return new Payment(this);
+        }
+    }
+
+
 }
--- src/main/java/za/ac/cput/domain/Session.java
+++ src/main/java/za/ac/cput/domain/Session.java
@@ -1,4 +1,144 @@
 package za.ac.cput.domain;
-
+/*
+    Session.java
+    Session builder Class
+    Author:Rohan Janzen Burger 230171990
+    Date:08/05/2025
+*/
 public class Session {
+    private String sessionid;//Initialize all neccesary attributes
+    private Passenger passenger;
+    private Driver driver;
+    private Location location;
+    private int passengerCount;
+    private float sessionAmount;
+    private String sessionStatus;
+    private Payment payment;
+
+    private Session() {//Default constructor
+
+    }
+
+    private Session(Builder builder) {//Constructor witht he paramenter of the builder
+        this.passenger = builder.passenger;//The Builder attributes are assigned the values from the builder pattern
+        this.driver = builder.driver;
+        this.location = builder.location;
+        this.passengerCount = builder.passengerCount;
+        this.sessionStatus = builder.sessionStatus;
+        this.sessionAmount = builder.sessionAmount;
+        this.sessionid = builder.sessionid;
+        this.payment = builder.payment;
+    }
+
+    public Passenger getPassenger() {//Get method retrieving the value
+        return passenger;
+    }
+
+    public String getSessionStatus() {
+        return sessionStatus;
+    }
+
+    public int getPassengerCount() {
+        return passengerCount;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public float getSessionAmount() {
+        return sessionAmount;
+    }
+
+    public String getSessionid() {
+        return sessionid;
+    }
+
+    @Override
+    public String toString() {//To String displays the attribute values as a string
+        return "============================"+'\n'+
+                "Session Object:"+'\n'+
+                "============================"+'\n'+
+                "Session id: " + sessionid +'\n'+
+                "PassengerCount: "+ passengerCount+ '\n'+
+                "Session Status: " + sessionStatus +'\n'+
+                "Session Amount: " + sessionAmount +'\n'+
+                passenger +'\n'+
+                driver +'\n'+
+                location+'\n'+
+                payment +'\n'+
+                "============================"+'\n';
+    }
+
+
+    public static class Builder {//Builder class contained inside of the domain class
+        private String sessionid;//Attributes for the builder class
+        private Passenger passenger;
+        private Driver driver;
+        private Location location;
+        private int passengerCount;
+        private String sessionStatus;
+        private float sessionAmount;
+        private Payment payment;
+
+
+        public Builder setPassenger(Passenger passenger) {//Method with paramters that sets the value of the attribute ,expects a return of type builder
+            this.passenger = passenger;
+            return this;
+
+        }
+
+        public Builder setDriver(Driver driver) {
+            this.driver = driver;
+            return this;
+        }
+
+        public Builder setLocation(Location location) {
+            this.location = location;
+            return this;
+
+        }
+
+        public Builder setPassengerCount(int passengerCount) {
+            this.passengerCount = passengerCount;
+            return this;
+
+        }
+
+
+        public Builder setSessionStatus(String sessionStatus) {
+            this.sessionStatus = sessionStatus;
+            return this;
+
+        }
+
+        public Builder setSessionAmount(float sessionAmount) {
+            this.sessionAmount = sessionAmount;
+            return this;
+        }
+
+        public Builder setSessionid(String sessionid) {
+            this.sessionid = sessionid;
+            return this;
+        }
+
+        public Builder setPayment(Payment payment) {
+            this.payment = payment;
+            return this;
+        }
+
+        public Session build() {//Method that returns/builds a type Session
+            return new Session(this);
+        }
+
+    }
+
 }
