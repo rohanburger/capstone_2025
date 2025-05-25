c3e41af9e6476d39f4970413fc209590cd4db26d
Kelsey-Jane Fabe <220328293@mycput.ac.za>
2025-05-25 19:50:03 +0200

    updated IService & added resource root folder for database purposes

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -2,11 +2,11 @@ package za.ac.cput.service;
 
 public interface IService <T, ID> {
 
-    T create(T t);
+    T create(T t); //creates a new entity object
 
-    T read(ID id);
+    T read(ID id); //read an object by its ID
 
-    T update(T t);
+    T update(T t); //updates an existing object
 
-    boolean delete(ID id);
+    boolean delete(ID id); //delete an object by its ID
 }
